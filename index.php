b4af12f1a221497b78cfd20345362d229f92b2ac
tvoja-mami <129745763+tvoja-mami@users.noreply.github.com>
2025-03-29 14:41:59 +0100

    v29.3.25/1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <div class="header">
+      </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>
